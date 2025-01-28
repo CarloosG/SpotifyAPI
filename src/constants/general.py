394e10219b6bd91ee00e91ac4ef875583fb664f6
CarloosG <frioold30@gmail.com>
2025-01-28 17:15:50 -0500

    Revert "modified general.py"
    This reverts commit 2f84ee7311b376f6f39b563e300c00d088c80276.

--- src/constants/general.py
+++ src/constants/general.py
@@ -6,5 +6,10 @@ class SpotifyConstants:
     CLIENT_ID: str = os.getenv("SPOTIFY_CLIENT_ID")
     CLIENT_SECRET: str = os.getenv("SPOTIFY_CLIENT_SECRET")
     TOKEN_URL: str = os.getenv("SPOTIFY_TOKEN_URL")
+    BASE_URL: str = "https://api.spotify.com/v1"
 
 
+
+# SPOTIFY_CLIENT_ID="15c528e05d8947f8bcc3d60683421505"
+# SPOTIFY_CLIENT_SECRET="7af4880026714be3a83441c3dddbe556"
+# SPOTIFY_TOKEN_URL="https://accounts.spotify.com/api/token"
--- src/handler/spotify_handler.py
+++ src/handler/spotify_handler.py
@@ -1,7 +1,6 @@
 import os
 import requests
 from constants.general import SpotifyConstants
-from utils.requester import Requester
 
 
 class SpotifyHandler:
@@ -13,12 +12,9 @@ class SpotifyHandler:
         
         def __init__(self):
             if not hasattr(self, "_initialized"):
-                self.constants = SpotifyConstants() 
-                self.token_url = self.constants.TOKEN_URL  
-                self.base_url = "https://api.spotify.com/v1"  
-                self.requester = Requester(max_retries=5, backoff_factor=2)
+                self.constants = SpotifyConstants()  
                 self.header_token = self.get_token()
-                self.headers = {"Authorization": f"Bearer {self.header_token}"}  
+                self.headers = {"Authorization": f"Bearer {self.get_token()}"}
                 self._initialized = True
 
         def get_token(self):
@@ -27,34 +23,39 @@ class SpotifyHandler:
             }
             data = {
                 "grant_type": "client_credentials",
-                "client_id": self.constants.CLIENT_ID,
-                "client_secret": self.constants.CLIENT_SECRET,
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
             }
-            response = self.requester.post(self.token_url, headers=headers)
-
+            response = requests.post(self.token_url, headers=headers, data=data)
+            if response.status_code == 200:
+                token = response.json().get("access_token")
+                return token
+            else:
+                raise Exception(f"Error al obtener el token: {response.status_code} - {response.text}")
+        
         def get_artist(self, artist_id: str):
             url = f"{self.base_url}/artists/{artist_id}"
-            response = self.requester.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers)
             return response.json()   
             
         def get_artist_music(self, artist_id:str):
             url=f"{self.base_url}/artist/{artist_id}/albums"
-            response = self.requester.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers)
             return response.json()  
         
         def get_song(self, song_id:str):
             url=f"{self.base_url}/tracks/{song_id}"
-            response = self.requester.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers)
             return response.json()
         
         def get_playlist(self, playlist_id: str):
             url = f"{self.base_url}/playlists/{playlist_id}"
-            response = self.requester.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers)
             return response.json()
         
         def get_country_new_releases(self, country_id:str):
             url=f"{self.base_url}/browse/new-releases?country={country_id}"
-            response = self.requester.get(url, headers=self.headers)
+            response = requests.get(url, headers=self.headers)
             return response.json()
         
 
@@ -66,7 +67,7 @@ class SpotifyHandler:
                 "limit": limit
             }
             url = f"{self.base_url}/search"
-            response = self.requester.get(url, headers=self.headers, params=params)
+            response = requests.get(url, headers=self.headers, params=params)
             return response.json()
 
 
