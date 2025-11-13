170058e9cf230e2d11e75f32d32dc2958a6bddf0
CarloosG <frioold30@gmail.com>
2025-11-12 22:24:52 -0500

    feat:first unit test

--- tests/test_spotify_handler.py
+++ tests/test_spotify_handler.py
@@ -6,16 +6,18 @@ from unittest.mock import patch, Mock
 
 
 class TestSpotifyHandler(unittest.TestCase):
-    @patch('requests.get')
-    def test_get_artist(self, mock_get):
+    @patch.object(SpotifyHandler, 'get_token', return_value='fake_token')
+    @patch('src.utils.requester.Requester.get')
+    def test_get_artist(self, mock_get, mock_get_token):
         mock_response = Mock()
         response_dict = {'id':'123','name':'Test Artist','genres':['pop'],'followers':{'total':1000},'popularity':50}
         mock_response.json.return_value = response_dict
-
         mock_get.return_value = mock_response
 
-        artist_data = SpotifyHandler.get_artist('123')
-        mock_get.assert_called_once_with('https://api.spotify.com/v1/artists/123')
+        spotify_handler = SpotifyHandler(Mock(), Mock())
+        artist_data = spotify_handler.get_artist('123')
+
+        mock_get.assert_called_once_with('https://api.spotify.com/v1/artists/123', headers=spotify_handler.headers)
         self.assertEqual(artist_data, response_dict)
 
 if __name__ == '__main__':
